1fc3b9ccf7c2b957d662e8f0c339176e1b360b6b
JosepMP <josepmarchesparra@gmail.com>
2022-08-28 17:06:35 +0200

    Mis tickets

--- src/components/Utils/ContentBox.js
+++ src/components/Utils/ContentBox.js
@@ -7,7 +7,7 @@ import { Flex } from '@chakra-ui/react';
 
 export default function ContentBox({...props}) {
     return (
-        <Flex flex={1} justifyContent={"center"} bg={props.bg ?? props.backgroundColor ?? "transparent"}>
+        <Flex flex={1} justifyContent={"center"} bg={props.bg ?? props.backgroundColor ?? "transparent"} >
             <Flex flex={1} direction={"column"} maxW={props.maxW ?? props.maxWidth ?? "1280px"} px={{base: "10px", md: "16px"}} {...props} bg={props.innerBg ?? props.innerBackgroundColor ?? "transparent"}>
                 {props.children}
             </Flex>
